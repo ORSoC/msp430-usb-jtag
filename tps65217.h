7a97f3658ea9af55b3b19794ac9d90ef2c31d4c6
Per Larsson <per.larsson@orsoc.se>
2012-11-05 09:02:25 +0000

    Added file header
    git-svn-id: http://svn.orsoc.se/svn/msp430-usb-jtag/trunk@12 2e7e0b7a-2549-41f0-8363-d79a98492be4

--- tps65217.h
+++ tps65217.h
@@ -1,9 +1,54 @@
-/*
- * tps65217.h
- *
- *  Created on: Oct 24, 2012
- *      Author: per
- */
+//////////////////////////////////////////////////////////////////////
+////                                                              ////
+//// TPS65217 control                                             ////
+////                                                              ////
+//// This file contains functions to control the power management ////
+//// IC TPS65217 from Texas Instruments.                          ////
+//// It uses swi2cmst.h and swi2cmst.c for I2C communtication     ////
+//// with the device.                                             ////
+////                                                              ////
+//// Description                                                  ////
+//// Implementation according to ref [1].                         ////
+////                                                              ////
+//// References                                                   ////
+//// [1] TPS65217A, TPS65217B, TPS65217C                          ////
+////     SINGLE-CHIP PMIC FOR BATTERY-POWERED SYSTEMS             ////
+////     SLVSB64E,  November 2011 – July 2012,                    ////
+////     Texas Instruments Incorporated                           ////
+////     http://www.ti.com/lit/gpn/tps65217a                      ////
+////                                                              ////
+//// To Do:                                                       ////
+////   -                                                          ////
+////                                                              ////
+//// Author(s):                                                   ////
+////   - Per Larsson, per.larsson@orsoc.se                        ////
+////                                                              ////
+//////////////////////////////////////////////////////////////////////
+////                                                              ////
+//// Copyright (C) 2012 Authors and ORSoC AB, Sweden              ////
+////                                                              ////
+//// This source file may be used and distributed without         ////
+//// restriction provided that this copyright statement is not    ////
+//// removed from the file and that any derivative work contains  ////
+//// the original copyright notice and the associated disclaimer. ////
+////                                                              ////
+//// This source file is free software; you can redistribute it   ////
+//// and/or modify it under the terms of the GNU Lesser General   ////
+//// Public License as published by the Free Software Foundation; ////
+//// either version 2.1 of the License, or (at your option) any   ////
+//// later version.                                               ////
+////                                                              ////
+//// This source is distributed in the hope that it will be       ////
+//// useful, but WITHOUT ANY WARRANTY; without even the implied   ////
+//// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR      ////
+//// PURPOSE. See the GNU Lesser General Public License for more  ////
+//// details.                                                     ////
+////                                                              ////
+//// You should have received a copy of the GNU Lesser General    ////
+//// Public License along with this source; if not, download it   ////
+//// from http://www.opencores.org/lgpl.shtml                     ////
+////                                                              ////
+//////////////////////////////////////////////////////////////////////
 
 #ifndef TPS65217_H_
 #define TPS65217_H_
