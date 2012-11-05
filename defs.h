34a110c3fc0dc7d48e3ae2f604a701f99472fa59
Per Larsson <per.larsson@orsoc.se>
2012-11-05 09:03:27 +0000

    Added file header
    git-svn-id: http://svn.orsoc.se/svn/msp430-usb-jtag/trunk@14 2e7e0b7a-2549-41f0-8363-d79a98492be4

--- defs.h
+++ defs.h
@@ -1,9 +1,52 @@
-/*
- * defs.h
- *
- *  Created on: Oct 29, 2012
- *      Author: per
- */
+//////////////////////////////////////////////////////////////////////
+////                                                              ////
+//// MSP430_USB_JTAG Definitions                                  ////
+////                                                              ////                                                              ////
+//// Description                                                  ////
+//// Definintions for general and board specific constants.       ////
+////                                                              ////
+//// References                                                   ////
+//// [1] MSP430F550x, MSP430F5510 Mixed Signal Microcontroller    ////
+////     SLAS645G, July 2009 - October 2012,                      ////
+////     Texas Instruments Incorporated                           ////
+////     http://www.ti.com/lit/gpn/msp430f5510                    ////
+////                                                              ////
+//// [2] OLIMEXINO-5510 Development Board User's Manual           ////
+////     https://www.olimex.com/Products/Duino/MSP430/            ////
+////           OLIMEXINO-5510/resources/OLIMEXINO-5510_Rev_A.pdf  ////
+////                                                              ////
+//// To Do:                                                       ////
+////   -                                                          ////
+////                                                              ////
+//// Author(s):                                                   ////
+////   - Per Larsson, per.larsson@orsoc.se                        ////
+////                                                              ////
+//////////////////////////////////////////////////////////////////////
+////                                                              ////
+//// Copyright (C) 2012 Authors and ORSoC AB, Sweden              ////
+////                                                              ////
+//// This source file may be used and distributed without         ////
+//// restriction provided that this copyright statement is not    ////
+//// removed from the file and that any derivative work contains  ////
+//// the original copyright notice and the associated disclaimer. ////
+////                                                              ////
+//// This source file is free software; you can redistribute it   ////
+//// and/or modify it under the terms of the GNU Lesser General   ////
+//// Public License as published by the Free Software Foundation; ////
+//// either version 2.1 of the License, or (at your option) any   ////
+//// later version.                                               ////
+////                                                              ////
+//// This source is distributed in the hope that it will be       ////
+//// useful, but WITHOUT ANY WARRANTY; without even the implied   ////
+//// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR      ////
+//// PURPOSE. See the GNU Lesser General Public License for more  ////
+//// details.                                                     ////
+////                                                              ////
+//// You should have received a copy of the GNU Lesser General    ////
+//// Public License along with this source; if not, download it   ////
+//// from http://www.opencores.org/lgpl.shtml                     ////
+////                                                              ////
+//////////////////////////////////////////////////////////////////////
 
 #ifndef DEFS_H_
 #define DEFS_H_
@@ -12,6 +55,7 @@
 
 //----------------------------------------------------------------------------
 // OLIMEXINO5510_ORSOC8695EP4GX specific definitions
+// See Ref [1] & [2]
 //----------------------------------------------------------------------------
 #if defined ( OLIMEXINO5510_ORSOC8695EP4GX )
 
@@ -44,6 +88,7 @@
 
 //----------------------------------------------------------------------------
 // ORDB3A specific definitions
+// See ref [1]
 //----------------------------------------------------------------------------
 #elif defined( ORDB3A )
 
