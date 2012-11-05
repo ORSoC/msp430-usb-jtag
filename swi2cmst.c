570723cb00572bc473e734e62602c6ac7eee42ee
Per Larsson <per.larsson@orsoc.se>
2012-11-05 09:00:30 +0000

    Added file header Added function swi2cmst_clrbus()
    git-svn-id: http://svn.orsoc.se/svn/msp430-usb-jtag/trunk@9 2e7e0b7a-2549-41f0-8363-d79a98492be4

--- swi2cmst.c
+++ swi2cmst.c
@@ -1,14 +1,68 @@
-/*
- * swi2cmst.c
- *
- *  Created on: Oct 24, 2012
- *      Author: per
- */
+//////////////////////////////////////////////////////////////////////
+////                                                              ////
+//// Software driven I2C Master                                   ////
+////                                                              ////
+//// This is a simple I2C Master for use when not all of the      ////
+//// advanced I2C featured are required.                          ////
+//// It does not require specific I2C hardware. Instead it does   ////
+//// bit-banging of GPIO ports.                                   ////
+////                                                              ////
+//// Description                                                  ////
+//// Implementation of the I2C protocol according to ref [1].     ////
+////                                                              ////
+//// Usage                                                        ////
+//// 1. Initialize by calling swi2cmst_init().                    ////
+//// 2. Optional: Clear potentially hung I2C slaves by calling    ////
+////    swi2cmst_clrbus(). Not guaranteed to work on all slaves.  ////
+////    Slaves may be hung if the CPU/MCU was reset during an     ////
+////    ongoing I2C transaction. There may also be other reasons. ////
+//// 3. Perform I2C accesses by calling one or more of            ////
+////    swi2cmst_wr1(), swi2cmst_wr2(), swi2cmst_rd1(),           ////
+////    swi2cmst_wr1rd1().                                        ////
+////                                                              ////
+//// References                                                   ////
+//// [1] UM10204 I2C-Bus Specification and User Manual            ////
+////     Rev.4, 13 February 2012, NXP B.V.                        ////
+////     http://www.nxp.com/documents/user_manual/um10204.pdf     ////
+////                                                              ////
+//// To Do:                                                       ////
+////  - Multi-master support (if and when there is a need)        ////
+////  - Functions for reading and writing data blocks with arrays ////
+////    (if and when there is a need)                             ////
+////  - More calls to SWI2CMST_DELAY here and there               ////
+////    (if there is a need)                                      ////
+////                                                              ////
+//// Author(s):                                                   ////
+////   - Per Larsson, per.larsson@orsoc.se                        ////
+////                                                              ////
+//////////////////////////////////////////////////////////////////////
+////                                                              ////
+//// Copyright (C) 2012 Authors and ORSoC AB, Sweden              ////
+////                                                              ////
+//// This source file may be used and distributed without         ////
+//// restriction provided that this copyright statement is not    ////
+//// removed from the file and that any derivative work contains  ////
+//// the original copyright notice and the associated disclaimer. ////
+////                                                              ////
+//// This source file is free software; you can redistribute it   ////
+//// and/or modify it under the terms of the GNU Lesser General   ////
+//// Public License as published by the Free Software Foundation; ////
+//// either version 2.1 of the License, or (at your option) any   ////
+//// later version.                                               ////
+////                                                              ////
+//// This source is distributed in the hope that it will be       ////
+//// useful, but WITHOUT ANY WARRANTY; without even the implied   ////
+//// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR      ////
+//// PURPOSE. See the GNU Lesser General Public License for more  ////
+//// details.                                                     ////
+////                                                              ////
+//// You should have received a copy of the GNU Lesser General    ////
+//// Public License along with this source; if not, download it   ////
+//// from http://www.opencores.org/lgpl.shtml                     ////
+////                                                              ////
+//////////////////////////////////////////////////////////////////////
 
 #include "swi2cmst.h"
-
-// TODO: More SWI2CMST_DELAY here and there
-// TODO: Multi-master support
 
 void swi2cmst_init(void)
 {
@@ -91,6 +145,23 @@ unsigned int swi2cmst_rdack( void )
 	}
 }
 
+unsigned int swi2cmst_clrbus( void )
+{
+	int i;
+
+	// At least nine STOPs, to release potentially hung slaves
+	for(i = 10; i != 0; i--) {
+	   SWI2CMST_SCL_LO;
+       swi2cmst_stop();
+	}
+	if (SWI2CMST_SDA_GET == 1 && SWI2CMST_SDA_GET == 1) {
+		return SWI2CMST_OK;
+	}
+	else {
+		return SWI2CMST_ERROR;
+	}
+}
+
 unsigned int swi2cmst_wr1(unsigned int slaveAddr, unsigned int data)
 {
 	unsigned int result;
@@ -164,5 +235,4 @@ unsigned int swi2cmst_wr1rd1(unsigned int slaveAddr, unsigned char wrData, unsig
 	return result;
 }
 
-// TODO: functions for writing and reading larger blocks of data with arrays (when there is a need).
 
