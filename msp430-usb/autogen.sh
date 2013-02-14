68d19493faa34d12a83baa08ce3cebbb1cd080c6
Yann Vernier <yann.vernier@orsoc.se>
2013-02-14 10:13:08 +0000

    msp430 usb code
    Based on MSP430USBDevleopersPackage_3_20_00 with modifications to work in GCC.
    Intend to extend it to allow defining a USB Blaster compatible interface. 
    
    
    
    
    git-svn-id: http://svn.orsoc.se/svn/msp430-usb-jtag/trunk@17 2e7e0b7a-2549-41f0-8363-d79a98492be4

---
+++ msp430-usb/autogen.sh
@@ -0,0 +1,6 @@
+aclocal --force
+autoheader -f
+libtoolize --copy --force
+automake -caf --foreign
+autoconf -f
+CC=msp430-gcc CFLAGS="-mmcu=msp430f5522" ./configure --target=msp430 --host=x86_64-unknown-linux-gnu
