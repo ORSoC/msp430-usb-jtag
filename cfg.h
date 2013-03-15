0c46dacba21df3cd972afef604488ace3b29e27d
Yann Vernier <yann.vernier@orsoc.se>
2013-03-15 13:47:44 +0000

    cfg.h: require board type from command line (in Makefile)
    git-svn-id: http://svn.orsoc.se/svn/msp430-usb-jtag/trunk@47 2e7e0b7a-2549-41f0-8363-d79a98492be4

--- cfg.h
+++ cfg.h
@@ -55,9 +55,12 @@
 
 // OLIMEX OLIMEXINO-5510 board (with an MSP430F5510 MCU) connected to an ORSOC_8695_EP4GX board as follows:
 // TODO: describe connections and modifications
-#define OLIMEXINO5510_ORSOC8695EP4GX
+//#define OLIMEXINO5510_ORSOC8695EP4GX
 
 // ORSoC ORDDB3A board
 //#define ORDB3A
+#if ! (defined(ORDB3A) || defined(OLIMEXINO5510_ORSOC8695EP4GX))
+#error Undefined board
+#endif
 
 #endif /* CFG_H_ */
