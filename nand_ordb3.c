36c4ed9dc1b635e173a6ea5407d771eac5aa7890
Yann Vernier <yann.vernier@orsoc.se>
2013-03-25 16:23:43 +0000

    Initial nand flash probe routine.
    After finding that there is ECC support in the Micron flash,
    I've stopped trying to get MTD or similar (next to try was 
    eCos flash support) to fit in the MSP430. 
    
    
    
    git-svn-id: http://svn.orsoc.se/svn/msp430-usb-jtag/trunk@63 2e7e0b7a-2549-41f0-8363-d79a98492be4

--- nand_ordb3.c
+++ nand_ordb3.c
@@ -1,12 +1,19 @@
 /* Routines to access NAND Flash memory on ORDB3 from MSP430 */
 
 /* I don't care that this is a msp430x, I know I have <64KiB */
+#if NAND_MTD
 typedef unsigned short phys_addr_t;
 typedef unsigned char u8;
 typedef unsigned short u16;
 
 #include <asm/errno.h>
 #include <linux/mtd/nand.h>
+
+#else
+
+#include <stdint.h>
+struct mtd_info; /* not used */
+#endif
 #include <msp430.h>
 
 // Port J is mapped as port 19 if you extrapolate from 1/2, 3/4 etc
@@ -86,6 +93,20 @@ static inline void nand_write_byte(uint8_t data) {
 	PJOUT |= WEn_BIT;
 }
 
+static inline void nand_ALE(int ale) {
+	if (ale)
+		PJOUT |= ALE_BIT;
+	else
+		PJOUT &= ~ALE_BIT;
+}
+
+static inline void nand_CLE(int cle) {
+	if (cle)
+		P5OUT |= CLE_BIT;
+	else
+		P5OUT &= ~CLE_BIT;
+}
+
 /* Functions ready for use from Linux/U-Boot MTD style code */
 
 static void ordb3_nand_write_buf(struct mtd_info *mtd, const uint8_t *buf, int len) {
@@ -106,6 +127,9 @@ static void ordb3_nand_read_buf(struct mtd_info *mtd, uint8_t *buf, int len) {
 	}
 }
 
+#ifndef EFAULT
+#define EFAULT 14
+#endif
 static int nand_verify_buf(struct mtd_info *mtd, const uint8_t *buf, int len) {
 	P1DIR = 0x00;
 	while (len--) {
@@ -132,19 +156,21 @@ static int nand_ready(struct mtd_info *mtd) {
 	return PJIN & R_Bn_BIT;
 }
 
+#if NAND_MTD
 static void cmd_ctrl(struct mtd_info *mtd, int cmd, unsigned int ctrl) {
 	if (cmd==NAND_CMD_NONE)
 		return;
 	if (ctrl & NAND_CLE) {
-		P5OUT |= CLE_BIT;
+		nand_CLE(1);
 		nand_write_byte(cmd);
-		P5OUT &= ~CLE_BIT;
+		nand_CLE(0);
 	} else {
-		PJOUT |= ALE_BIT;
+		nand_ALE(1);
 		nand_write_byte(cmd);
-		PJOUT &= ~ALE_BIT;
+		nand_ALE(0);
 	}
 }
+#endif
 
 /* Default function uses a command instead */
 static void select_chip(struct mtd_info *mtd, int chip) {
@@ -155,6 +181,7 @@ static void select_chip(struct mtd_info *mtd, int chip) {
 	}
 }
 
+#if NAND_MTD
 int board_nand_init(struct nand_chip *nand) {
 	nand->read_byte=ordb3_nand_read_byte;
 	nand->write_buf=ordb3_nand_write_buf;
@@ -164,4 +191,96 @@ int board_nand_init(struct nand_chip *nand) {
 	nand->cmd_ctrl=cmd_ctrl;
 	nand->dev_ready=nand_ready;
 	return 0;
+}
+#endif
+
+int nand_probe(char *buf, int size) {
+	int tries=1+5, i, timeout;
+
+#if NAND_MTD
+	struct mtd_info mtd;
+	struct nand_chip nand;
+	mtd.priv = &nand;
+	board_nand_init(&nand);
+#endif
+	if (size<4+1+32)
+		return 0;  /* Caller error */
+
+	nand_init();
+
+	select_chip(0, 0);
+	
+	/* Read ID */
+	nand_CLE(1);
+	nand_write_byte(0x90);
+	nand_CLE(0);
+	nand_ALE(1);
+	nand_write_byte(0x20);
+	nand_ALE(0);
+	ordb3_nand_read_buf(0, buf, 4);
+	
+	if (!(buf[0]=='O' &&
+	      buf[0]=='N' &&
+	      buf[0]=='F' &&
+	      buf[0]=='I')) {
+		buf[0]='F';
+		buf[1]='a';
+		buf[2]='i';
+		buf[3]='l';
+		return 4;
+	}
+
+	do {
+		/* Read ID vendor - to identify chip */
+		nand_CLE(1);
+		nand_write_byte(0x90);  // Read ID
+		nand_CLE(0);
+		nand_ALE(1);
+		nand_write_byte(0x00);  // manufacturer ID region
+		nand_ALE(0);
+		ordb3_nand_read_buf(0, buf+4, 5);
+
+		if (!(buf[4]==0x2c && buf[5]==0xf1)) {
+			buf[4]='!';
+			break;  // Not the MT29F1G08ABADA chip! Don't poke ECC..
+		}
+
+		if (buf[4+4]&0x80)
+			break;  // ECC is enabled
+
+		/* ECC is not enabled, try to enable it */
+		nand_CLE(1);
+		nand_write_byte(0xef);  // set features
+		nand_CLE(0);
+		nand_ALE(1);
+		nand_write_byte(0x90);  // Micron array operation mode
+		nand_ALE(0);
+		nand_write_byte(0x08);  // Enable internal ECC
+		nand_write_byte(0x00);
+		nand_write_byte(0x00);
+		nand_write_byte(0x00);
+		for (timeout=-1; timeout && !nand_ready(0); --timeout)
+			;  // Wait until flash chip ready
+		if (!nand_ready(0))
+			return 5;
+	} while (--tries);
+
+	/* We have now initialized and hopefully enabled ECC. Read the description */
+	nand_CLE(1);
+	nand_write_byte(0xec);  // Read parameter page
+	nand_CLE(0);
+	nand_ALE(1);
+	nand_write_byte(0x00);
+	nand_ALE(0);
+	for (timeout=-1; timeout && !nand_ready(0); --timeout)
+		;  // Wait until flash chip ready
+	if (!nand_ready(0))
+		return 5;
+	// Skip bytes until human readable manufacturer and model
+	for (i=0; i<32; i++) {
+		ordb3_nand_read_byte(0);
+	}
+	// Read ID strings
+	ordb3_nand_read_buf(0, buf+4+1, 32);
+	return 4+1+32;
 }
